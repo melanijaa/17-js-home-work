d8b13099ccd2bdeb6fda7be1bdf695503798826b
Melanija <melanija.sibakovska1@gmail.com>
2022-04-01 23:55:01 +0300

    ciklai

--- kestutis.js/03-30.js
+++ kestutis.js/03-30.js
@@ -1,14 +1,15 @@
 /*
-1. Žinomas skaičius n. Parašykite programą, kuri nustatytų ar skaičius yra blogas ar geras? Neigiamas –„Blogas“, teigiamas „Geras“. Pasitikrinkite: Kai n = 20, turėtume gauti: Skaičius geras.
+1. Žinomas skaičius n. Parašykite programą, kuri nustatytų ar skaičius yra blogas ar geras? Neigiamas  –„Blogas“, teigiamas „Geras“. Pasitikrinkite: Kai n = 20, turėtume gauti: Skaičius geras.
 */
 
-const n = 20;
-console.log(n)
+const n = -6;
 
-if ( n === 20) {
+if ( n > 0) {
     console.log('Skaičius geras.');
-} else {
+} else if ( n < 0 ) {
     console.log('Skaičius blogas');
+} else {
+    console.log('n lygu nulis');
 }
 
 /*
@@ -33,13 +34,11 @@ if (šviesoforas === 3) {
 
 const d = 3;
 const k = 18;
-const nu = 5;
+const nu = 9;
 
-if (d === 2 && k === 8 && nu === 5) {
+if (k <= d * nu) {
     console.log('Knygos tepla į dėžes.')
-}
-
-if (d === 3 && k === 18 && nu === 5) {
+} else {
     console.log('Knygos netelpa į dėžes.')
 }
 
@@ -47,45 +46,37 @@ if (d === 3 && k === 18 && nu === 5) {
 4. Duoti du sveikieji skaičiai a ir b. Parašykite programą, kuri didesnį skaičių pamažintu vienetu, o mažesnį padidintu vienetu.
 */
 
-const a = 6;
-const b = 13;
+let a = 13;
+let b = 13;
 
-let mažesnisSkaičius = a;
-console.log(--mažesnisSkaičius);
-console.log(--mažesnisSkaičius);
-console.log(--mažesnisSkaičius);
-console.log(--mažesnisSkaičius);
-console.log(--mažesnisSkaičius);
-console.log(--mažesnisSkaičius);
-console.log(--mažesnisSkaičius);
-
-let didesnisSkaičius = b;
-console.log(++didesnisSkaičius);
-console.log(++didesnisSkaičius);
-console.log(++didesnisSkaičius);
-console.log(++didesnisSkaičius);
-console.log(++didesnisSkaičius);
+if (a > b) {
+    a--;
+    b++;
+    console.log(`${a} ${b}`);
+} else if (a < b) {
+    a++;
+    b--;
+    console.log(`${a} ${b}`);
+} else {
+    console.log('Skaiciai yra lygus.');
+}
 
 /*
 5. Saulius sutaupė c centų ir už juos nusprendė nusipirkti ledų. Parduotuvėje porcija ledų kainuoja p centų. Parašykite programą, kuri suranda, kiek porcijų ledų f nusipirks Saulius ir kiek centų m dar liks. Pasitikrinkite Kai c=50, p=20, turi būti : Saulius nusipirks 2 porcijas, liks centų 10. Gali prireikti panaudoti Math.floor() funkciją.
 */
 
-const sutaupė = 50;
-const ledųKaina = 20;
-const kiekGaliNusipirkti = 2; 
-const kiekLiksPinigų = 10;
+const sutaupė = 70;
+const ledųKaina = 10;
 
-console.log(Math.floor(sutaupė/ledųKaina));
-console.log(Math.floor(sutaupė-(ledųKaina*kiekGaliNusipirkti)));
+console.log('Saulius gales nusipirkti ' + Math.floor(sutaupė/ledųKaina) + ' porcijas ledu ir jam liks ' + (sutaupė % ledųKaina) + ' centu ');
 
 /*
 6. Yra n degtukų. Parašykite programą, kuri nustatytų, ar iš tų degtukų galima sudėti lygiakraštį trikampį. Dėliojamai figūrai turi būti panaudoti visi degtukai. Degtukų laužyti negalima. Pasitikrinkite: jei n = 6, turi būti: Trikampį sudėlioti galima. jei n = 4, turi būti: Trikampį sudėlioti negalima.
 */
 
-const degtukai = 4;
-console.log(degtukai)
+const degtukai = 12;
 
-if (degtukai === 6) {
+if (degtukai % 3 === 0 && degtukai > 0) {
     console.log('Trikampį sudėlioti galima.')
 } else {
     console.log('Trikampį sudėlioti negalima.')
@@ -95,71 +86,39 @@ if (degtukai === 6) {
 7. Parašykite programą, kuri žinant mėnesio numerį n atspausdintų to mėnesio dienų kiekį d. (tarkime, kad metai yra paprastieji) (pvz. n=5, ats. d=31; n=2 ats. d=28).
 */
 
-const menesioPavadinimas = 'kovas';
-
-if (menesioPavadinimas === 'vasaris') {
-    console.log('29d.');
+const men = 9;
+if (men === 2){
+    console.log('28');
+} else if (men === 4 || men === 6 || men === 9 || men === 11) {
+    console.log('30');
 } else {
-    if (menesioPavadinimas === 'kovas') {
-        console.log('31d.');
-    } else {
-        if (menesioPavadinimas === 'balandis') {
-            console.log('30d.');
-        } else {
-            if (menesioPavadinimas === 'geguze') {
-                console.log('31d.');
-            } else {
-                if (menesioPavadinimas === 'birzelis')
-                console.log('30d.');
-            } 
-        }
-    }
+    console.log('31');
 }
 
 /*
 8.Du broliai nesutaria, kuriam tvarkyti kambarį. Jie meta kauliuką. Jei iškrenta skaičius 1, 3 arba 5 – kambarį tvarko jaunėlis, jei 2, 4 arba 6 – vyresnėlis. Parašykite programą, kuri įvedus iškritusį skaičių k, ekrane rodytų pranešimą „Kambarį tvarkys jaunėlis“, arba „Kambarį tvarkys vyresnėlis“.Pasitikrinkite: kai k = 1, ekrane turi būti: Kambarį tvarkys jaunėlis. Kai k = 4, ekrane turi būti: Kambarį tvarkys vyresnėlis.
 */
 
-const kauliukas = 4;
+const kauliukas = 1;
 
-if (kauliukas === 1) {
-    console.log('Kambarį tvarkys jaunėlis.')
+if (kauliukas % 2 === 1) {
+    console.log('Kambarį tvarkys jaunėlis.');
 } else {
-    if (kauliukas === 2) {
-        console.log('Kambarį tvarkys vyresnėlis.')
-    } else {
-        if (kauliukas === 3) {
-            console.log('Kambarį tvarkys jaunėlis.')
-        } else {
-            if (kauliukas === 4) {
-                console.log('Kambarį tvarkys vyresnėlis.')
-            } else {
-                if (kauliukas === 5) {
-                    console.log('Kambarį tvarkys jaunėlis.')
-                } else {
-                    if ( kauliukas === 6) {
-                        console.log('Kambarį tvarkys vyresnėlis.')
-                    }
-                }
-            }
-        }
-    }
-
+    console.log('Kambarį tvarkys vyresnėlis.');
 }
 
 /*
 9. Pirmosios olimpinės žaidynės įvyko 1896 metais ir toliau organizuojamos kas ketveri metai. Jei žaidynės neįvyksta, tie metai vis tiek laikomi olimpiniais, o žaidynėms skiriamas eilės numeris. Parašykite programą, kuri surastų m-ųjų metų olimpinių žaidynių numerį n. Jei metai neolimpiniai, turi būti spausdinama „Metai neolimpiniai“. Pasitikrinkite. Kai m = 1904, turi būti spausdinama: n = 3. Kai m = 2005, turi būti spausdinama: Metai neolimpiniai.
 */
 
-const metai = 1906;
+const metai = 2012;
 
-if ((metai - 1896) % 4 === 0) {
-    nunu = 1 + ((metai - 1896) / 4);
-    console.log(`${metai} metais vyko ${nunu}-os rungtynes.`)
+if (metai % 4 === 0) {
+    const nunu = 1 + ((metai - 1896) / 4);
+    console.log(`${metai} metais vyko ${nunu}-os rungtynes.`);
 } else {
-    console.log(`Metai neolimpiniai.`)
+    console.log(`Metai neolimpiniai.`);
 }
-
 
 /*
 10.Petras išėjo iš namų, kai laikrodis rodė v1 valandų ir m1 minučių. Į gimnaziją Petro kelionė trunka m2 minučių. Parašykite programą, kuri ekrane parodytų pranešimą apie tai, ar Petras nepavėluos į pamoką, prasidedančią v valandų ir m minučių. Pasitikrinkite: jei v1 = 8, m1 = 29, m2 = 43, v = 9, m = 5, turi būti: Petras į pamoką pavėluos; jei v1 = 8, m1 = 29, m2 = 23, v = 9, m = 5, turi būti: Petras į pamoką nepavėluos.
@@ -167,17 +126,15 @@ if ((metai - 1896) % 4 === 0) {
 
 const v1 = 8;
 const m1 = 29;
-const m2 = 23;
-const v = 9;
+const m2 = 36;
+const v3 = 9;
 const m3 = 5;
 
-if (v1 === 8 && m1 === 29 && m2 ===23 && v ===9 && m3 ===5) {
-    console.log('Petras į pamoką nepavėluos.')
+if (v1 * 60 + m1 + m2 <= v3 * 60 + m3) {
+    console.log('Petras į pamoką nepavėluos.');
+} else {
+    console.log('Petras į pamoką pavėluos.');
 }
-
-if (v1 === 8 && m1 === 29 && m2 ===43 && v ===9 && m3 ===5) {
-    console.log('Petras į pamoką pavėluos.')
-} 
 
 
 
--- kestutis.js/04-01.ciklas.js
+++ kestutis.js/04-01.ciklas.js
@@ -2,6 +2,100 @@
 1.Naudojant ciklą atspausdinti penkias eilutes ‘Labas’.
 */
 
+for(let i = 0; i < 5; i++) {
+    console.log('Labas');
+}
+
 /*
-2.Naudojant ciklą penkiose eilutėse atspausdinti skaičius 0 1 2 3 4 (vienas skaičius vienoje eilutėje).3.Naudojant ciklą penkiose eilutėse atspausdinti skaičius 0 10 20 30 40 (vienas skaičius vienoje eilutėje).4.Naudojant ciklą penkiose eilutėse atspausdinti skaičius 49 50 51 52 53 (vienas skaičius vienoje eilutėje).5.Naudojant ciklą penkiose eilutėse atspausdinti skirtingus atsitiktinius skaičius nuo 0 iki 10 (vienas skaičius vienoje eilutėje).6.Naudojant ciklą, atspausdinti skaičius nuo 1 iki 20. Nespausdinti skaičių, kurie dalinasi iš 3.7.Parašykite programą, kuri atspausdintų įvesto skaičiaus daugybos lentelę nuo 1 iki 10.8.Vienas colis yra 2,54 cm. Sudarykite programą kuri pateiktų ekranelentelę nuo 1 iki n colius centimetrais.9.Bankas moka 2% metinių palūkanų. Indėlininkas pasidėjo į banką 100e. Sudarykite kasmetinę banko atskaitą klientui iki n metų. Ataskaitoje pateikite: metus, palūkanas, indėlį.10.Keliamieji metai turi 366 dienas, paprastieji–365. Visi metai, išskyrus šimtmečius, yra keliamieji, jie dalūs iš 4. Šimtmečių metai yra keliamieji, jeigu dalūs 400. Pvz.: 1600 metai yra keliamieji, nes dalūs iš 400, o 1700 –paprastieji. Raskite visus mūsų eros keliamuosius metus.function rand(min, max){Return Math.floor(Math.random()*(max-min+1)+min);}
+2.Naudojant ciklą penkiose eilutėse atspausdinti skaičius 0 1 2 3 4 (vienas skaičius vienoje eilutėje).
+*/
+
+for(let i = 0; i <5; i++) {
+    console.log(i);
+}
+
+/*
+3.Naudojant ciklą penkiose eilutėse atspausdinti skaičius 0 10 20 30 40 (vienas skaičius vienoje eilutėje).
+*/
+
+for(let i = 0; i <= 40; i+=10) {
+    console.log(i);
+}
+
+/*
+4.Naudojant ciklą penkiose eilutėse atspausdinti skaičius 49 50 51 52 53 (vienas skaičius vienoje eilutėje).
+*/
+
+for(let i = 49; i <= 53; i++) {
+    console.log(i);
+}
+
+/*
+5.Naudojant ciklą penkiose eilutėse atspausdinti skirtingus atsitiktinius skaičius nuo 0 iki 10 (vienas skaičius vienoje eilutėje).
+*/
+
+function rand(min, max){return Math.floor(Math.random()*(max-min+1)+min);}
+for (let i = 0; i < 5; i++) {
+     console.log(rand(0,10));
+ }
+
+/*
+6.Naudojant ciklą, atspausdinti skaičius nuo 1 iki 20. Nespausdinti skaičių, kurie dalinasi iš 3.
+*/
+console.log('------6-----------');
+
+for(let i = 1; i <= 20 ; i++) {
+    if(i % 3 !== 0){
+       console.log(i);
+    }
+}
+
+/*
+7.Parašykite programą, kuri atspausdintų įvesto skaičiaus daugybos lentelę nuo 1 iki 10.
+*/
+
+let number = 4;
+    
+for (let i = 1; i <= 10; i++) {
+    console.log(number, "X", i, "=", number * i);
+}
+
+/*
+8.Vienas colis yra 2,54 cm. Sudarykite programą kuri pateiktų ekrane lentelę nuo 1 iki n colius centimetrais.
+*/
+
+let colis = 12;
+
+for (let i = 1; i <= 12; i++) {
+    console.log(i , 'coliu', '=', i * 2.54, 'cm');
+}
+
+/*
+9.Bankas moka 2% metinių palūkanų. Indėlininkas pasidėjo į banką 100e. Sudarykite kasmetinę banko atskaitą klientui iki n metų. Ataskaitoje pateikite: metus, palūkanas, indėlį.
+*/
+
+const palūkanuNorma = 2;
+const metai = 10;
+let indelis = 100;
+
+for (let i = 1; i <= metai; i++) {
+    const palūkanos = (Math.round(indelis * palūkanuNorma)) / 100;
+    indelis += palūkanos;
+    console.log(`Po ${i} metu indelis bus ${indelis.toFixed(2)}, o palukanos bus ${palūkanos}.`);
+}
+
+/*
+10.Keliamieji metai turi 366 dienas, paprastieji–365. Visi metai, išskyrus šimtmečius, yra keliamieji, jie dalūs iš 4. Šimtmečių metai yra keliamieji, jeigu dalūs 400. Pvz.: 1600 metai yra keliamieji, nes dalūs iš 400, o 1700 –paprastieji. Raskite visus mūsų eros keliamuosius metus.
+*/
+
+for (let m = 0; m <= 2022; m+=4) {
+    if ( m % 4 === 0 && m % 100 !== 0 || m % 400 === 0 ) {
+        console.log(`${m} yra keliamieji.`);
+    } 
+}
+
+/*
+else {
+        console.log(`${m} nera keliamieji.`);
+    }
 */
