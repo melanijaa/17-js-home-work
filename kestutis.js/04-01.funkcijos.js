031c06b200da406b273f546f407b7e20f666ead9
Melanija <melanija.sibakovska1@gmail.com>
2022-04-06 13:10:12 +0300

    home work

--- kestutis.js/04-01.funkcijos.js
+++ kestutis.js/04-01.funkcijos.js
@@ -2,5 +2,124 @@
 1.Parašyti funkciją, kuri priima du skaičius. Grąžina tų skaičių sumą.
 */
 
+const suma = (a, b) => a + b;
+const ats = suma(2, 5);
+console.log(ats);
+
 /*
-2.Parašyti funkciją, kuri priima du kintamuosius.Palygina tuos du skaičius ir grąžina rezultatą ar pirmas skaičius yra didesnis, ar antras, ar jie yra lygūs.3.Parašyti funkciją, kuri priimtų vieną skaičių. Turi būti grąžinamas rezultatas ar tie metai yra keliemieji.4.Parašyti funkciją, kuri priimtų vieną skaičių. Grąžinti skaičiaus kvadratą.5.Parašyti funkciją, kuri priimtų vieną skaičių, kuris turi būti didesnis už vienetą. Funkcija turi grąžinti visų skaičių sumą, nuo 1 iki įvesto skaičiaus.6.Parašykite funkciją, kuri priimtų skaičių ir suskaičiuotų, iš kiek sveikų skaičių jos argumentas dalijasi be liekanos (išskyrus vienetą ir patį save).7.Parašyti funkciją, kuri priimtų vieną kintamąjį-tekstą. Funkcija turi išvesti tekstą į ekraną ir dar papildomai parodyti jo ilgį(simbolių kiekį).8.Parašyti funkcijątelefonoNumeris, kuri priima vieną kintamąjį–masyvą su 10 skaičių jame.Funkcija turi grąžinti telefono numerį tokiu formatu -"(XXX) XXX-XXXX".9.Parašyti funkciją, kuri priimtų du kintamuosius-tekstus.Palyginti kuris tekstas yra ilgesnis.10.Parašyti funkciją, kuri priimtų vieną kintamąjį-tekstą.Suskaičiuoti, kiek tekste yra ‚a‘ raidžių.
+2.Parašyti funkciją, kuri priima du kintamuosius.Palygina tuos du skaičius ir grąžina rezultatą ar pirmas skaičius yra didesnis, ar antras, ar jie yra lygūs.
+*/
+
+function compare (c, d) {
+    if (c > d) {
+        return `pirmas skaičius: "${c}", yra didesnis`;
+    } else if (c < d) {
+        return `antras skaičius: "${d}", yra didesnis`;
+    } else {
+        return `skaičiai yra lygus`;
+    }
+}
+const rez = compare(4, 6);
+console.log(rez);
+
+/*
+3.Parašyti funkciją, kuri priimtų vieną skaičių. Turi būti grąžinamas rezultatas ar tie metai yra keliemieji.
+*/
+
+function metai(m) {
+    if (m % 4 === 0 && m % 100 !== 0) {
+        return `${m} metai yra keliemieji.`;
+    } else {
+        return `${m} metai nėra keliemieji.`;
+    }
+}
+const result = metai (2004);
+console.log(result);
+
+/*
+4.Parašyti funkciją, kuri priimtų vieną skaičių. Grąžinti skaičiaus kvadratą.
+*/
+
+const kvadratas = (e) => e * e;
+const res = kvadratas(89);
+console.log(res);
+
+/*
+5.Parašyti funkciją, kuri priimtų vieną skaičių, kuris turi būti didesnis už vienetą. Funkcija turi grąžinti visų skaičių sumą, nuo 1 iki įvesto skaičiaus.
+*/
+
+function visuSkaiciuSuma (f) {
+    let sum = 0;
+    for (let i = 1; i <= f; i++) {
+        sum += i;
+    }
+    return sum;
+}
+console.log(visuSkaiciuSuma(815));
+
+/*
+6.Parašykite funkciją, kuri priimtų skaičių ir suskaičiuotų, iš kiek sveikų skaičių jos argumentas dalijasi be liekanos (išskyrus vienetą ir patį save).
+*/
+
+function liekana(g, h, j) {
+    let count = 0;
+    for (let i = g; i <= h; i++) {
+        if (i % j === 0) {
+            count++
+        }
+    }
+    return `Sveikų skaičių intervale nuo ${g} iki ${h}, besidalijančių be liekanos (išskyrus vienetą ir patį save) iš ${j} yra ${count} vienetai.`;
+}
+
+console.log(liekana(-34, 678, 3));
+
+/*
+7.Parašyti funkciją, kuri priimtų vieną kintamąjį-tekstą. Funkcija turi išvesti tekstą į ekraną ir dar papildomai parodyti jo ilgį(simbolių kiekį).
+*/
+
+const tekstas1 = 'Bananai, mandarinai ir obuoliai yra nemėgstamiausi mano mamos vaisiai.'
+function tekstoIlgis (tekstas1) {
+    for (i = 0; i <= tekstas1.length; i++) {
+        return `${tekstas1} ${tekstas1.length}`
+    }
+}
+
+/*
+8.Parašyti funkcijątelefonoNumeris, kuri priima vieną kintamąjį–masyvą su 10 skaičių jame.Funkcija turi grąžinti telefono numerį tokiu formatu -"(XXX) XXX-XXXX".
+*/
+
+
+
+/*
+9.Parašyti funkciją, kuri priimtų du kintamuosius-tekstus.Palyginti kuris tekstas yra ilgesnis.
+*/
+
+function text (text1, text2) {
+    if (text1 > text2) {
+        return 'Pirmas tekstas yra ilgesnis.';
+    } else if (text1 < text2) {
+        return 'Antras tekstas yra ilgesnis.';
+    } else {
+        return 'Abu tekstai yra lygūs.';
+    }
+}
+const atsText = text('Ananasas ir arbūzas yra mano mamos mėgstamiausi vaisiais.', 'Bananai, mandarinai ir obuoliai yra nemėgstamiausi mano mamos vaisiai.');
+console.log(atsText);
+
+/*
+10.Parašyti funkciją, kuri priimtų vieną kintamąjį-tekstą.Suskaičiuoti, kiek tekste yra ‚a‘ raidžių.
+*/
+
+
+/*
+const tekstas = 'Ananasas ir arbūzas yra mano mamos mėgstamiausi vaisiais'
+let count = 0;
+for ( let i = 0; i <= tekstas.length; i++) {
+    if ( i === 'a') {
+        count += 1
+    }
+}
+console.log(count);
+const text1 = 'Ananasas ir arbūzas yra mano mamos mėgstamiausi vaisiais.'
+const text2 = 'Bananai, mandarinai ir obuoliai yra nemėgstamiausi mano mamos vaisiai.'
+*/
---
+++ rimantas.js/04.06-class.js
@@ -0,0 +1,75 @@
+/*
+Class extends
+
+Class Dog {}
+žino vardą
+žino rūšies pavadinimą
+žino (kailio) spalvą
+žino akių spalvą
+žino kojų skaičių
+žino garsą kurį gali išskleisti
+žino mėgstamą maistą
+žino ar šiuo metu sėdi (default: ne)
+gali išskleisti garsą, pvz.: “Rex: au au!!”
+gali prisistatyti, pvz.: “Hi, I am a dog and my name is Rex. My color is black. My eyes are blue. I have 4 legs. Right now I am standing.”
+gali atsisėsti ir apie tai pranešti, pvz.: “Rex: I am sitting.”
+gali atsistoti ir apie tai pranešti, pvz.: “Rex: I am standing.”
+
+Class Cat {}
+žino vardą
+žino rūšies pavadinimą
+žino (kailio) spalvą
+žino akių spalvą
+žino kojų skaičių
+žino garsą kurį gali išskleisti
+žino mėgstamą maistą
+žino ar šiuo metu guli (default: ne)
+gali išskleisti garsą, pvz.: “Rainis: miau miau!!”
+gali prisistatyti, pvz.: “Hi, I am a cat and my name is Rainis. My color is black. My eyes are blue. I have 4 legs. Right now I am standing.”
+gali sudaužyti daiktą, pvz.: “Rainis just destroyed a cake.”
+gali atsigulti ir apie tai pranešti, pvz.: “Rex: I am laying down.”
+gali atsistoti ir apie tai pranešti, pvz.: “Rex: I am standing.”
+
+Class Hamster {}
+žino vardą
+žino rūšies pavadinimą
+žino (kailio) spalvą
+žino akių spalvą
+žino kojų skaičių
+žino garsą kurį gali išskleisti
+žino mėgstamą maistą
+žino ar šiuo metu valgo (default: ne)
+gali išskleisti garsą, pvz.: “Chamiakas: krimst krimst!!”
+gali prisistatyti, pvz.: “Hi, I am a hamster and my name is Chamiakas. My color is black. My eyes are blue. I have 4 legs.”
+gali pradėti valgyti ir apie tai pranešti, pvz.: “Rainis just destroyed a cake.”
+
+Class Parrot {}
+žino vardą
+žino rūšies pavadinimą
+žino (plunksnų) spalvą
+žino akių spalvą
+žino sparnų skaičių
+žino garsą kurį gali išskleisti
+žino kokius žodžius moka ištarti (default: nieko)
+gali išskleisti garsą, pvz.: “Jack: I am captain Jack!!”
+gali prisistatyti, pvz.: “Hi, I am a parrot and my name is Jack. My color is black. My eyes are blue. I have 2 wings.”
+gali išmokti naują žodį ir jį ištarti, pvz.: “Jack: word.”
+gali ištarti visus išmoktus žodžius, pvz.: “Jack: word, post, car, bike.”
+Class Chicken {}
+žino vardą
+žino rūšies pavadinimą
+žino (plunksnų) spalvą
+žino akių spalvą
+žino sparnų skaičių
+žino garsą kurį gali išskleisti
+gali išskleisti garsą, pvz.: “Little: cyp cyp!!”
+gali prisistatyti, pvz.: “Hi, I am a chicken and my name is Little. My color is black. My eyes are blue. I have 2 wings.”
+Class Pike {}
+žino vardą
+žino rūšies pavadinimą
+žino (žvynų) spalvą
+žino akių spalvą
+žino garsą kurį gali išskleisti
+gali išskleisti garsą, pvz.: “Little: cyp cyp!!”
+gali prisistatyti, pvz.: “Hi, I am a chicken and my name is Little. My color is black. My eyes are blue. I have 2 wings.”
+*/
