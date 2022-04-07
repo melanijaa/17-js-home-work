e13c1f6fd19e1b91bbfbe3091d17aa57f8fc8895
Melanija <melanija.sibakovska1@gmail.com>
2022-04-07 18:56:05 +0300

    funkcijos

--- kestutis.js/04-01.funkcijos.js
+++ kestutis.js/04-01.funkcijos.js
@@ -15,11 +15,13 @@ function compare (c, d) {
         return `pirmas skaičius: "${c}", yra didesnis`;
     } else if (c < d) {
         return `antras skaičius: "${d}", yra didesnis`;
-    } else {
+    } else if (c === d) {
         return `skaičiai yra lygus`;
+    } else {
+        return `ERROR`
     }
 }
-const rez = compare(4, 6);
+const rez = compare('friday', 6);
 console.log(rez);
 
 /*
@@ -27,7 +29,7 @@ console.log(rez);
 */
 
 function metai(m) {
-    if (m % 4 === 0 && m % 100 !== 0) {
+    if (m % 4 === 0 && m % 100 !== 0 || 400 === 0) {
         return `${m} metai yra keliemieji.`;
     } else {
         return `${m} metai nėra keliemieji.`;
@@ -51,7 +53,7 @@ console.log(res);
 function visuSkaiciuSuma (f) {
     if (f > 1) {
         let sum = 0;
-        for (let i = 0; i <= f; i++) {
+        for (let i = 1; i <= f; i++) {
             sum += i;
         } return sum;
     }
@@ -62,16 +64,16 @@ console.log(visuSkaiciuSuma(100));
 6.Parašykite funkciją, kuri priimtų skaičių ir suskaičiuotų, iš kiek sveikų skaičių jos argumentas dalijasi be liekanos (išskyrus vienetą ir patį save).
 */
 
-function liekana(g, h, j) {
+function liekana(g) {
     let count = 0;
-    for (let i = g; i <= h; i++) {
-        if (i % j === 0) {
+    for (let i = 0; i < g; i++) {
+        if (g % i === 0 && i !== 1) {
             count++
         }
     }
-    return `Sveikų skaičių intervale nuo ${g} iki ${h}, besidalijančių be liekanos (išskyrus vienetą ir patį save) iš ${j} yra ${count} vienetai.`;
+    return `Skaičius ${g} be liekanos dalinasi iš ${count} sk.`
 }
-console.log(liekana(0, 13, 3));
+console.log(liekana(37485));
 
 /*
 7.Parašyti funkciją, kuri priimtų vieną kintamąjį-tekstą. Funkcija turi išvesti tekstą į ekraną ir dar papildomai parodyti jo ilgį(simbolių kiekį).
@@ -86,10 +88,11 @@ console.log(tekstoIlgis('Bananai, mandarinai ir obuoliai yra nemėgstamiausi man
 8.Parašyti funkciją telefonoNumeris, kuri priima vieną kintamąjį–masyvą su 10 skaičių jame. Funkcija turi grąžinti telefono numerį tokiu formatu -"(XXX) XXX-XXXX".
 */
 
-function telefonoNumeris (tel, efo, no) {
-    return `Mano telefono numeris yra (${tel})${efo}-${no}`
-}
-console.log(telefonoNumeris(860, 653, 9177))
+const n = [3, 7, 0, 6, 0, 6, 5, 3, 9, 1];
+function telefonoNumeris(n) {
+    return `Mano telefono numeris yra (${n[0]}${n[1]}${n[2]})${n[3]}${n[4]}${n[5]}-${n[6]}${n[7]}${n[8]}${n[9]}.`
+} 
+console.log(telefonoNumeris(n))
 
 /*
 9.Parašyti funkciją, kuri priimtų du kintamuosius-tekstus.Palyginti kuris tekstas yra ilgesnis.
@@ -100,8 +103,10 @@ function text (text1, text2) {
         return 'Pirmas tekstas yra ilgesnis.';
     } else if (text1 < text2) {
         return 'Antras tekstas yra ilgesnis.';
-    } else {
+    } else if (text1 === text2) {
         return 'Abu tekstai yra lygūs.';
+    } else {
+        return `ERROR`
     }
 }
 const atsText = text('Ananasas ir arbūzas yra mano mamos mėgstamiausi vaisiais.', 'Bananai, mandarinai ir obuoliai yra nemėgstamiausi mano mamos vaisiai.');
@@ -111,6 +116,19 @@ console.log(atsText);
 10.Parašyti funkciją, kuri priimtų vieną kintamąjį-tekstą.Suskaičiuoti, kiek tekste yra ‚a‘ raidžių.
 */
 
+const tekstas2 = ('Ananasas ir arbūzas yra mano mamos mėgstamiausi vaisiais.');
+function count(tekstas2) {
+    let count = 0;
+    for (let i = 0; i < tekstas2.length; i++) {
+        if((tekstas2[i] === 'A' || tekstas2[i] === 'a'))
+        count++
+    } return `Tekste yra ${count} "a" raidžių`
+}
+
+console.log(count(tekstas2));
+
+//pornografija
+/*
 function suskaiciuok2(tekstas2) {
     const container = {};
     for (let i = 0; i < tekstas2.length; i++) {
@@ -119,5 +137,4 @@ function suskaiciuok2(tekstas2) {
     }
     return container;
 }
-
-console.log(suskaiciuok2('ananasas ir arbūzas yra mano mamos mėgstamiausi vaisiais.'));
+*/
--- kestutis.js/04-05.while.js
+++ kestutis.js/04-05.while.js
@@ -2,6 +2,11 @@
 1.Generuoti atsitiktinius skaičius nuo 1 iki 10 tol, kol skaičių suma bus mažesnė už 100.
 */
 
+function rand(min, max){return Math.floor(Math.random()*(max-min+1)+min);}
+for (let i = 1; i < 10; i++) {
+     console.log(rand(0,100));
+}
+
 /*
 2.Generuoti atsitiktinius skaičius nuo 1 iki 10 tol, kol sugeneruotų skaičių suma bus dali iš 7.
 /*
@@ -13,14 +18,12 @@ Kontroliniai duomenys:
 Įveskite knygos skyrių skaičių: 8
 Tadas visą knygą perskaitys per 4 dienas (-ų).
 Tadas vidutiniškai per dieną perskaitė 2 skyrius(-ų).
-Įveskite knygos skyrių skaičių: 17.
-Tadas visą knygą perskaitys per 6 dienas (-ų).
-Tadas vidutiniškai per dieną perskaitė 2.83 skyrius (-ų).
+Įveskite knygos skyrių skaičių: 17.Tadas visą knygą perskaitys per 6 dienas (-ų).Tadas vidutiniškai per dieną perskaitė 2.83 skyrius (-ų).
 */
 
 /*
 4.Naudojant ciklą atspausdinti skirtingus atsitiktinius skaičius nuo 0 iki 10 (vienas skaičius vienoje eilutėje). Paskutinis atspausdintas skaičius turi būti 5.
-/*
+*/
 
 /*
 5. Naudojant ciklą atspausdinti skirtingus atsitiktinius skaičius nuo 0 iki 10 (vienas skaičius vienoje eilutėje). Paskutinis atspausdintas skaičius turi būti 5 arba 7; Suskaičiuoti kiek ciklų prasisuko.
